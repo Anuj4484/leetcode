d52eb6f031671cd0ab05e06430eb8ad61839b666
Anuj KR <112244284+Anuj4484@users.noreply.github.com>
2024-07-23 11:45:51 +0530

    Time: 8 ms (62.32%), Space: 45.5 MB (18.65%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -7,11 +7,13 @@ class Solution {
         }
 
         Arrays.sort(heights);
-        for(int i=0;i<n/2;i++){
+
+        for( int i=0;i<n/2;i++){
             int temp = heights[i];
             heights[i] = heights[n-1-i];
             heights[n-1-i] = temp;
         }
+
         for(int i=0;i<n;i++){
             names[i] = mapping.get(heights[i]);
         }
